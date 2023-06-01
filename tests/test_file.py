761fe3e1c31b7a012c137c43dbe6fc30b4ab88d1
J. Nick Koston <nick@koston.org>
2023-06-01 14:46:18 -0500

    fix: add missing cover for file (#43)

--- tests/test_file.py
+++ tests/test_file.py
@@ -1,5 +1,8 @@
+import os
 from pathlib import Path
+from unittest.mock import patch
 
+import py
 import pytest
 
 from index_503.file import write_utf8_file
@@ -11,3 +14,35 @@ def test_write_utf8_file(tmp_path: Path) -> None:
 
     with pytest.raises(OSError):
         write_utf8_file(Path("/not-writable"), "bar")
+
+
+def test_write_utf8_file_fails_at_rename(
+    tmpdir: py.path.local, caplog: pytest.LogCaptureFixture
+) -> None:
+    """Test that if rename fails not not remove, we do not log the failed cleanup."""
+    test_dir = tmpdir.mkdir("files")
+    test_file = Path(test_dir / "test.json")
+
+    with pytest.raises(OSError), patch(
+        "index_503.file.os.replace", side_effect=OSError
+    ):
+        write_utf8_file(test_file, '{"some":"data"}', False)
+
+    assert not os.path.exists(test_file)
+
+    assert "File replacement cleanup failed" not in caplog.text
+
+
+def test_write_utf8_file_fails_at_rename_and_remove(
+    tmpdir: py.path.local, caplog: pytest.LogCaptureFixture
+) -> None:
+    """Test that if rename and remove both fail, we log the failed cleanup."""
+    test_dir = tmpdir.mkdir("files")
+    test_file = Path(test_dir / "test.json")
+
+    with pytest.raises(OSError), patch(
+        "index_503.file.os.remove", side_effect=OSError
+    ), patch("index_503.file.os.replace", side_effect=OSError):
+        write_utf8_file(test_file, '{"some":"data"}', False)
+
+    assert "File replacement cleanup failed" in caplog.text
