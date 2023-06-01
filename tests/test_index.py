a18f9adc3a57c3daaf4a46528dd680ac33cfbb93
J. Nick Koston <nick@koston.org>
2023-06-01 14:00:02 -0500

    chore: add end to end test

---
+++ tests/test_index.py
@@ -0,0 +1,45 @@
+import json
+from pathlib import Path
+from shutil import copyfile
+
+from index_503.index import make_index
+from index_503.wheel_file import WHEEL_FILE_VERSION
+
+from . import FIXTURES
+
+TEST_WHEELS = (
+    "sphinxcontrib.applehelp-1.0.3-py3-none-any.whl",
+    "CO2Signal-0.4.2-py3-none-any.whl",
+)
+
+
+def test_make_index_end_to_end(tmp_path: Path) -> None:
+    """Test make_index() end to end."""
+    origin_path = tmp_path.joinpath("musllinux")
+    origin_path.mkdir()
+    origin_path_index = tmp_path.joinpath("musllinux-index")
+
+    for test_wheel in TEST_WHEELS:
+        fixture_wheel_path = FIXTURES.joinpath(test_wheel)
+        origin_wheel_path = origin_path.joinpath(test_wheel)
+        copyfile(fixture_wheel_path, origin_wheel_path)
+
+    for _ in range(2):
+        assert make_index(origin_path) == origin_path_index
+
+        json_cache_path = origin_path_index.joinpath("cache.json")
+        assert json_cache_path.exists()
+        assert origin_path_index.joinpath("index.html").exists()
+        assert origin_path_index.joinpath("CO2Signal").exists()
+        assert origin_path_index.joinpath("CO2Signal", "index.html").exists()
+        assert json.loads(json_cache_path.read_text()) == {
+            "CO2Signal-0.4.2-py3-none-any.whl": {
+                "version": WHEEL_FILE_VERSION,
+                "canonical_name": "co2signal",
+                "filename": "CO2Signal-0.4.2-py3-none-any.whl",
+                "metadata_name": "CO2Signal",
+                "wheel_hash": "23976d5df4ac7b4b85210e4f334382d65b759a2c3795121f9688e64365ef4790",
+                "requires_python": None,
+                "metadata_hash": "a6e73c9cf4f9469c5b308830afbc000bb806df5d894598dd499737e94974c27c",
+            }
+        }
