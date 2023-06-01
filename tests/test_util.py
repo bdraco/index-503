3b8481ae8a39cb0e91ea6044085b573b2deb66a3
J. Nick Koston <nick@koston.org>
2023-06-01 14:42:08 -0500

    feat: add tests for exclusive_lock

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,4 +1,10 @@
-from index_503.util import get_sha256_hash
+from pathlib import Path
+from threading import Lock, Thread
+from time import sleep
+
+import pytest
+
+from index_503.util import exclusive_lock, get_sha256_hash
 
 from . import FIXTURES
 
@@ -9,3 +15,29 @@ def test_get_sha256_hash() -> None:
         get_sha256_hash(metadata_path)
         == "a448e2bbca19bfdb6cd8ed15aa59c05f04e010640d4404dd6c2c062be2336999"
     )
+
+
+def test_exclusive_lock(tmp_path: Path, caplog: pytest.LogCaptureFixture) -> None:
+    thread_lock = Lock()
+
+    class LockedRunner(Thread):
+        def run(self):
+            with exclusive_lock(tmp_path):
+                assert thread_lock.acquire(False) is True
+                sleep(0.05)
+                thread_lock.release()
+
+    thread_1 = LockedRunner()
+    thread_2 = LockedRunner()
+    thread_3 = LockedRunner()
+    thread_1.start()
+    thread_2.start()
+    thread_3.start()
+
+    assert "Another instance is running" in caplog.text
+
+    thread_1.join()
+    thread_2.join()
+    thread_3.join()
+
+    assert "Another instance finished, continuing" in caplog.text
