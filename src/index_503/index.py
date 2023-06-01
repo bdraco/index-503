b1ac9e148e8cd5c625c2e0857947d5b602bbd8fa
J. Nick Koston <nick@koston.org>
2023-06-01 07:55:55 -0500

    chore: reduce code from refactor

--- src/index_503/index.py
+++ src/index_503/index.py
@@ -76,9 +76,6 @@ class IndexMaker:
 
     def make_index(self) -> Tuple[Path, Dict[str, List["WheelFile"]]]:
         """Generate a simple repository of Python wheels."""
-        old_index = self.old_index
-        target_path = self.target_path
-
         with tempfile.TemporaryDirectory(
             dir=str(self.target_path_parent), ignore_cleanup_errors=True
         ) as temp_dir:
@@ -87,16 +84,16 @@ class IndexMaker:
             self._make_index_at_temp_dir(temp_dir_path)
             self._atomic_replace_old_index(temp_dir_path)
 
-            if old_index:
-                rmtree(old_index)
+            if self.old_index:
+                rmtree(self.old_index)
 
-            return target_path, self.projects
+            return self.target_path, self.projects
 
     def _atomic_replace_old_index(self, temp_dir_path: Path) -> None:
         """Atomically replace the old index with the new one."""
-        target_path = self.target_path
-
-        final_name = target_path.parent / (target_path.name + "-" + temp_dir_path.name)
+        final_name = self.target_path.parent / (
+            self.target_path.name + "-" + temp_dir_path.name
+        )
         final_build_name = final_name.parent / (final_name.name + "-build")
 
         # Rename the new index to the final name
@@ -106,46 +103,35 @@ class IndexMaker:
         os.symlink(final_name, final_build_name)
 
         # Finally replace the live index with the new one
-        os.replace(final_build_name, target_path)
+        os.replace(final_build_name, self.target_path)
 
     def _make_index_at_temp_dir(self, temp_dir_path: Path) -> None:
         """Generate a simple repository of Python wheels in a temp dir."""
-        origin_path = self.origin_path
-        origin_name = self.origin_name
-        projects = self.projects
-        cache = self.cache
-        raw_cache = cache.cache
-        target_path = self.target_path
-        all_wheel_files = self.all_wheel_files
-        canonical_name_to_metadata_name = self.canonical_name_to_metadata_name
-        new_wheel_file_objects = self.new_wheel_file_objects
-        wheel_file_name_to_metadata_path = self.wheel_file_name_to_metadata_path
-
-        for wheel_file in glob.glob(str(origin_path.joinpath("*.whl"))):
+        for wheel_file in glob.glob(str(self.origin_path.joinpath("*.whl"))):
             wheel_path = Path(wheel_file)
             wheel_file_name = wheel_path.name
             target_file = temp_dir_path.joinpath(wheel_file_name)
             metadata_path = target_file.with_suffix(f"{target_file.suffix}.metadata")
-            wheel_file_symlink_target = f"../{origin_name}/{wheel_file_name}"
-            wheel_cache = raw_cache.get(wheel_file_name)
-            all_wheel_files.add(wheel_file_name)
+            wheel_file_symlink_target = f"../{self.origin_name}/{wheel_file_name}"
+            wheel_cache = self.cache.cache.get(wheel_file_name)
+            self.all_wheel_files.add(wheel_file_name)
 
             if wheel_cache and wheel_cache["version"] == WHEEL_FILE_VERSION:
                 wheel_file_obj = WheelFile(**wheel_cache)
-                copyfile(target_path.joinpath(metadata_path.name), metadata_path)
+                copyfile(self.target_path.joinpath(metadata_path.name), metadata_path)
             else:
                 maybe_wheel_file_obj = WheelFile.from_wheel(wheel_path, metadata_path)
                 if not maybe_wheel_file_obj:
                     continue
                 wheel_file_obj = maybe_wheel_file_obj
-                wheel_file_name_to_metadata_path[wheel_file_name] = metadata_path
-                new_wheel_file_objects.append(wheel_file_obj)
-                raw_cache[wheel_file_name] = asdict(wheel_file_obj)
+                self.wheel_file_name_to_metadata_path[wheel_file_name] = metadata_path
+                self.new_wheel_file_objects.append(wheel_file_obj)
+                self.cache.cache[wheel_file_name] = asdict(wheel_file_obj)
 
             canonical_name = wheel_file_obj.canonical_name
             metadata_name = wheel_file_obj.metadata_name
-            projects[metadata_name].append(wheel_file_obj)
-            canonical_name_to_metadata_name[canonical_name] = metadata_name
+            self.projects[metadata_name].append(wheel_file_obj)
+            self.canonical_name_to_metadata_name[canonical_name] = metadata_name
             os.symlink(wheel_file_symlink_target, target_file)
 
         self.repair_metadata_files()
@@ -154,35 +140,30 @@ class IndexMaker:
 
     def remove_old_wheel_files(self) -> None:
         # Remove any old wheel files from the cache
-        all_wheel_files = self.all_wheel_files
-        cache = self.cache
-        raw_cache = cache.cache
-        removed_wheels = set(raw_cache.keys()) - all_wheel_files
+        removed_wheels = set(self.cache.cache.keys()) - self.all_wheel_files
         for old_wheel_file in removed_wheels:
-            del raw_cache[old_wheel_file]
+            del self.cache.cache[old_wheel_file]
 
     def repair_metadata_files(self) -> None:
         """Repair the metadata files."""
-        canonical_name_to_metadata_name = self.canonical_name_to_metadata_name
-        wheel_file_name_to_metadata_path = self.wheel_file_name_to_metadata_path
-        new_wheel_file_objects = self.new_wheel_file_objects
         # Now fix all the metadata files and update the sha256 hash + cache
-        for wheel_file_obj in new_wheel_file_objects:
+        for wheel_file_obj in self.new_wheel_file_objects:
             wheel_file_name = wheel_file_obj.filename
-            metadata_path = wheel_file_name_to_metadata_path[wheel_file_name]
+            metadata_path = self.wheel_file_name_to_metadata_path[wheel_file_name]
 
-            if repair_metadata_file(metadata_path, canonical_name_to_metadata_name):
+            if repair_metadata_file(
+                metadata_path, self.canonical_name_to_metadata_name
+            ):
                 wheel_file_obj.metadata_hash = get_sha256_hash(metadata_path)
                 self.cache.cache[wheel_file_name] = asdict(wheel_file_obj)
 
     def generate_index_pages(self, temp_dir_path: Path) -> None:
         """Generate the index pages."""
-        projects = self.projects
-        index_content = str(generate_index(projects.keys()))
+        index_content = str(generate_index(self.projects.keys()))
         write_utf8_file(temp_dir_path.joinpath("index.html"), index_content)
         project_base_url = URL("../")
 
-        for project_name, project_files in projects.items():
+        for project_name, project_files in self.projects.items():
             project_dir = temp_dir_path.joinpath(canonicalize_name(project_name))
             project_dir.mkdir(exist_ok=True)
             project_index = generate_project_page(
