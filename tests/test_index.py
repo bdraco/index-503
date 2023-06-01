33a88755e73bd81316d4b96e3ef02bc234374ab6
J. Nick Koston <nick@koston.org>
2023-06-01 14:05:33 -0500

    chore: add coverage (#36)

--- tests/test_index.py
+++ tests/test_index.py
@@ -29,9 +29,17 @@ def test_make_index_end_to_end(tmp_path: Path) -> None:
 
         json_cache_path = origin_path_index.joinpath("cache.json")
         assert json_cache_path.exists()
-        assert origin_path_index.joinpath("index.html").exists()
+        assert origin_path_index.joinpath(
+            "CO2Signal-0.4.2-py3-none-any.whl.metadata"
+        ).exists()
+        project_index_path = origin_path_index.joinpath("index.html")
+        assert project_index_path.exists()
+        project_index_html = project_index_path.read_text()
+        assert "CO2Signal" in project_index_html
+        assert "/co2signal/" in project_index_html
         assert origin_path_index.joinpath("CO2Signal").exists()
-        assert origin_path_index.joinpath("CO2Signal", "index.html").exists()
+        co2signal_index_path = origin_path_index.joinpath("CO2Signal", "index.html")
+        assert co2signal_index_path.exists()
         assert json.loads(json_cache_path.read_text()) == {
             "CO2Signal-0.4.2-py3-none-any.whl": {
                 "version": WHEEL_FILE_VERSION,
@@ -43,3 +51,13 @@ def test_make_index_end_to_end(tmp_path: Path) -> None:
                 "metadata_hash": "a6e73c9cf4f9469c5b308830afbc000bb806df5d894598dd499737e94974c27c",
             }
         }
+
+        co2signal_index_html = co2signal_index_path.read_text()
+        assert (
+            "CO2Signal-0.4.2-py3-none-any.whl#sha256=23976d5df4ac7b4b85210e4f334382d65b759a2c3795121f9688e64365ef4790"
+            in co2signal_index_html
+        )
+        assert (
+            'data-dist-info-metadata="sha256=a6e73c9cf4f9469c5b308830afbc000bb806df5d894598dd499737e94974c27c"'
+            in co2signal_index_html
+        )
