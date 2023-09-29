43d9c1f5c86ed873381284de909e7043bd8992b8
J. Nick Koston <nick@koston.org>
2023-09-29 18:16:29 +0100

    fix: metadata is missing

--- tests/test_index.py
+++ tests/test_index.py
@@ -18,21 +18,27 @@ TEST_WHEELS = (
     "CO2Signal-0.4.2-py3-none-any.whl",
 )
 
+TEST_WHEELS_NAME_CHANGE = (
+    "alexapy-1.26.8-py3-none-any.whl",
+    "alexapy-1.26.9-py3-none-any.whl",
+    "alexapy-1.27.0-py3-none-any.whl",
+)
 
-def install_wheels(origin_path: Path) -> None:
+
+def install_wheels(origin_path: Path, files: Tuple[str, ...]) -> None:
     """Install wheels for testing."""
-    for test_wheel in TEST_WHEELS:
+    for test_wheel in files:
         fixture_wheel_path = FIXTURES.joinpath(test_wheel)
         origin_wheel_path = origin_path.joinpath(test_wheel)
         copyfile(fixture_wheel_path, origin_wheel_path)
 
 
-def setup_wheels(tmp_path: Path) -> Tuple[Path, Path]:
+def setup_wheels(tmp_path: Path, files: Tuple[str, ...]) -> Tuple[Path, Path]:
     """Setup wheels for testing."""
     origin_path = tmp_path.joinpath("musllinux")
     origin_path.mkdir()
 
-    install_wheels(origin_path)
+    install_wheels(origin_path, files)
 
     origin_path_index = tmp_path.joinpath("musllinux-index")
 
@@ -41,7 +47,7 @@ def setup_wheels(tmp_path: Path) -> Tuple[Path, Path]:
 
 def test_make_index_fails(tmp_path: Path) -> None:
     """Test make index cleans up on failure."""
-    origin_path, _ = setup_wheels(tmp_path)
+    origin_path, _ = setup_wheels(tmp_path, TEST_WHEELS)
     parent_dir = origin_path.parent
 
     with pytest.raises(ValueError), patch(
@@ -57,7 +63,7 @@ def test_make_index_fails(tmp_path: Path) -> None:
 
 def test_make_index_end_to_end(tmp_path: Path) -> None:
     """Test make_index() end to end."""
-    origin_path, origin_path_index = setup_wheels(tmp_path)
+    origin_path, origin_path_index = setup_wheels(tmp_path, TEST_WHEELS)
 
     for _ in range(2):
         assert make_index(origin_path) == origin_path_index
@@ -184,4 +190,83 @@ def test_make_index_end_to_end(tmp_path: Path) -> None:
         # Now put back the original wheels
         # to make sure the next run will regenerate the index
         # and hashes along with size
-        install_wheels(origin_path)
+        install_wheels(origin_path, TEST_WHEELS)
+
+
+def test_make_index_with_name_change(tmp_path: Path) -> None:
+    """Test make_index() end to end with a name change in the wheel."""
+    origin_path, origin_path_index = setup_wheels(tmp_path, TEST_WHEELS_NAME_CHANGE)
+
+    for _ in range(2):
+        assert make_index(origin_path) == origin_path_index
+
+        json_cache_path = origin_path_index.joinpath("cache.json")
+        assert json_cache_path.exists()
+        assert origin_path_index.joinpath(
+            "alexapy-1.26.8-py3-none-any.whl.metadata"
+        ).exists()
+        assert origin_path_index.joinpath(
+            "alexapy-1.26.9-py3-none-any.whl.metadata"
+        ).exists()
+        assert origin_path_index.joinpath(
+            "alexapy-1.27.0-py3-none-any.whl.metadata"
+        ).exists()
+        project_index_path: Path = origin_path_index.joinpath("index.html")
+        assert project_index_path.exists()
+        assert project_index_path.stat().st_mode & 0o0777 == 0o644
+        parent_path: Path = project_index_path.parent
+        assert parent_path.stat().st_mode & 0o0777 == 0o755
+        project_index_html = project_index_path.read_text()
+        assert "alexapy" in project_index_html
+        assert "/alexapy/" in project_index_html
+        alexapy_path: Path = origin_path_index.joinpath("alexapy")
+        assert alexapy_path.stat().st_mode & 0o0777 == 0o755
+        assert alexapy_path.exists()
+        alexapy_index_path: Path = origin_path_index.joinpath("alexapy", "index.html")
+        assert alexapy_index_path.exists()
+        assert alexapy_index_path.stat().st_mode & 0o0777 == 0o644
+        print(json_cache_path.read_text())
+        assert json.loads(json_cache_path.read_text()) == {
+            "alexapy-1.26.9-py3-none-any.whl": {
+                "version": 10,
+                "canonical_name": "alexapy",
+                "filename": "alexapy-1.26.9-py3-none-any.whl",
+                "metadata_name": "alexapy",
+                "wheel_hash": "dd4eed7a53f4932156ba779c73c5bd64286ee4d0f714028401e8f9acd9cd6afd",
+                "requires_python": ">=3.10,<4",
+                "metadata_hash": "dc54cefafe6853e984d9651f0f69c61eb1c292cb94677a3766b605692fb5c126",
+                "mtime": ANY,
+                "size": 45566,
+            },
+            "alexapy-1.27.0-py3-none-any.whl": {
+                "version": 10,
+                "canonical_name": "alexapy",
+                "filename": "alexapy-1.27.0-py3-none-any.whl",
+                "metadata_name": "AlexaPy",
+                "wheel_hash": "a6dc55daac51d0f8f423b85cec346fd012b2f58a5b05493524a93156585c5110",
+                "requires_python": ">=3.11,<4",
+                "metadata_hash": "6c989ef90d200a9105d6e2dcd9c32c1caa84022061f6d0160a2d9fb7d442a816",
+                "mtime": ANY,
+                "size": 49851,
+            },
+            "alexapy-1.26.8-py3-none-any.whl": {
+                "version": 10,
+                "canonical_name": "alexapy",
+                "filename": "alexapy-1.26.8-py3-none-any.whl",
+                "metadata_name": "alexapy",
+                "wheel_hash": "f17ac40d033ca869706f899fe0ab518c3c44459c951c7141d3be0117158c0fe2",
+                "requires_python": ">=3.10,<4",
+                "metadata_hash": "8ea579b5b6ebc80657f3bc3d5046af91a208f342b4dae19966d52eafacf82fec",
+                "mtime": ANY,
+                "size": 45560,
+            },
+        }
+        alexapy_index_html = alexapy_index_path.read_text()
+        assert (
+            "alexapy-1.27.0-py3-none-any.whl#sha256=a6dc55daac51d0f8f423b85cec346fd012b2f58a5b05493524a93156585c5110"
+            in alexapy_index_html
+        )
+        assert (
+            'data-dist-info-metadata="sha256=a6e73c9cf4f9469c5b308830afbc000bb806df5d894598dd499737e94974c27c"'
+            in alexapy_index_html
+        )
