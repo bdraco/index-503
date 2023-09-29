0a72b574293401222d76bd0ad567d1ccfd90b415
J. Nick Koston <nick@koston.org>
2023-09-29 16:49:53 +0100

    fix: tests

--- tests/test_wheelfile.py
+++ tests/test_wheelfile.py
@@ -1,5 +1,7 @@
 from pathlib import Path
+from unittest.mock import ANY
 
+from index_503.util import get_mtime_and_size_from_path
 from index_503.wheel_file import WHEEL_FILE_VERSION, WheelFile
 
 from . import FIXTURES
@@ -18,13 +20,19 @@ def test_wheel_file(tmp_path: Path) -> None:
         "metadata_name": "CO2Signal",
         "wheel_hash": "23976d5df4ac7b4b85210e4f334382d65b759a2c3795121f9688e64365ef4790",
         "requires_python": None,
+        "size": 2841,
+        "mtime": ANY,
         "metadata_hash": "a6e73c9cf4f9469c5b308830afbc000bb806df5d894598dd499737e94974c27c",
     }
     to_cache = wheel_file_obj.as_dict()
-    assert WheelFile.from_cache(to_cache) == wheel_file_obj
+    mtime, size = get_mtime_and_size_from_path(wheel_path)
+    assert WheelFile.from_cache(to_cache, mtime, size) == wheel_file_obj
+
+    # Wrong mtime should not use the cache
+    assert WheelFile.from_cache(to_cache, 42, size) is None
 
     to_cache["version"] = 0
-    assert WheelFile.from_cache(to_cache) is None
+    assert WheelFile.from_cache(to_cache, mtime, size) is None
 
 
 def test_wheel_file_with_missing_metadata(tmp_path: Path) -> None:
